de620ac4c00edc1999114a184540f1b113b53fe9
Vreij-Lal <vreijlal@gmail.com>
2024-04-12 22:36:49 +0300

    single linked list append function

--- Single Linked Lists.c
+++ Single Linked Lists.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void append();
 
 void main(){
 
+struct node {
+    int data;
+    struct node* link;
+};
+
+
+struct node* root = NULL;
+}
+
+void append(){
+    struct node* temp;
+    temp = (struct node*)malloc(sizeof(struct node));
+    printf("Enter node data: ");
+    scanf("%d", &temp -> data);
+    temp -> link = NULL;
+    if(root == NULL){
+        root = temp;
+    }
+    else{
+        struct node* p;
+        p = root;
+        while(p -> link != NULL){
+            p = p -> link;
+        }
+        p -> link = temp;
+    }
 }
 
