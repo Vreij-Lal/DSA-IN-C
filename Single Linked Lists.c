9afa6ffd30230d66420304084ba7b048afa59deb
Vreij-Lal <vreijlal@gmail.com>
2024-04-30 22:53:39 +0300

    quiz append function

--- Single Linked Lists.c
+++ Single Linked Lists.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void append();
+
 //linked list definition
 struct node {
     int data;
@@ -21,5 +23,24 @@ void main(){
     printf("5.Display\n");
     printf("6.Delete\n");
     printf("7.Quit\n");
+}
+
+void append(){
+    struct node *temp *p;
+    temp = (struct node*)mallco(sizeof(struct node));
+    printf("enter element number: ");
+    scanf("%d", &temp -> data);
+    temp -> link = NULL;
+    if(root == NULL){
+        root = temp;
+    }
+    else{
+        p = root;
+        while(p -> link != NULL){
+            p = p -> link;
+        }
+        p -> link = temp;
+    }
+
 }
 
