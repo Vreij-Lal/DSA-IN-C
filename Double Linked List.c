caa506398986ddd9a8fa91fa2f9d942824ed320a
Vreij-Lal <vreijlal@gmail.com>
2024-06-06 23:50:17 +0300

    double linked list append function

--- Double Linked List.c
+++ Double Linked List.c
@@ -9,6 +9,29 @@ struct node {
 
 struct node *root = NULL;
 
+void append();
+
 void main(){
 
+}
+
+void append(){
+    struct node *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
+    printf("Enter value");
+    scanf("%d", &temp -> data);
+    temp->left = NULL;
+    temp->right = NULL;
+    if(root == NULL){
+        root = temp;
+    }
+    else{
+        struct node *p;
+        p = root;
+        while(p->right != NULL){
+            p = p -> right;
+        }
+        p -> right = temp;
+        temp -> left = p;
+    }
 }
